1575fe33d3c19400c54dacabb92e4642d15acda5
Finura99 <aryaelpula@gmail.com>
2026-06-16 17:55:28 +0100

    Added edge case for the get_trades test

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,4 +7,9 @@ client = TestClient(app)
 def test_get_trades():
     response = client.get("/trades?limit=1")
 
-    assert response.status_code in [200,404] # checks if whether db is empty or has data
+    assert response.status_code in [200,404] # checks if whether db has data or is empty
+
+def test_get_trades_invalid_limit_returns_422():
+    response = client.get("/trades?limit=101")
+
+    assert response.status_code == 422
